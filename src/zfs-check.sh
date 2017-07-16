fe4988109e37528be26fc585d1661cbe2d2c7f69
Francis Chin <dev@fchin.com>
2017-07-16 03:19:40 +0100

    Changed default operation mode and added switches
    Extensive update to allow output to STDOUT for command line usage
    Switches to override default values and behaviour
    New summary mode
    Report format tweaks to regularise messages

--- src/zfs-check.sh
+++ src/zfs-check.sh
@@ -1,74 +1,145 @@
 #!/bin/bash
 
+# zfs-check
+# Copyright (C) 2017 Francis Chin <dev@fchin.com>
+
 REPORT=/tmp/zfs-check-$$.log
-LOGFILE=/var/log/zfs-check.log
+
+log() { # level, message
+  local LEVEL=$1
+  shift 1
+  case $LEVEL in
+    (err*)
+      echo -e "zfs-check error: $*" | tee -a $REPORT >&2
+      MAILNOTIFY=yes
+      ;;
+    (war*)
+      echo -e "zfs-check warning: $*" >> $REPORT
+      MAILNOTIFY=yes
+      ;;
+    (sum*) echo -e "$*" >> $REPORT ;;
+    (inf*) if [ -z "${SUMMARY}" ]; then echo -e "$*" >> $REPORT; fi ;;
+  esac
+}
+
+#
+# Options
+#
+DEFAULTLOG=/var/log/zfs-check.log
 MAXCAPACITY=95
-MAILNOTIFY=false
-MAILRECIPIENT="fchin.uk@gmail.com"
+ZFSAUTOSNAPLABEL=zfs-auto-snap_
 
-echo -e "Starting zfs-check on `date`\n" >> $REPORT
+print_usage() {
+  echo "Usage: $0 [-l|-L LOGFILE] [-m ADDRESS] [-c CAPACITY] [-s]
+Use ZFS utilities to log the health and status of all mounted pools, and
+optionally alert via email if there are any warnings.
 
-mail_report() { # subject
-  local EMAILSUBJECT="$*"
-  cat $REPORT | mail -s "${EMAILSUBJECT}" $MAILRECIPIENT
+  -l           Instead of sending output to STDOUT, append to a log at
+               the default location ${DEFAULTLOG}
+
+  -L LOGFILE   Append output to LOGFILE instead of STDOUT. 
+
+  -m ADDRESS   Email warnings to ADDRESS.
+
+  -c CAPACITY  Use a zpool utilisation upper limit of CAPACITY% instead
+               of the default upper limit ${MAXCAPACITY}%.
+
+  -s           Summary mode.
+" >&2
+exit 2
+}
+
+while getopts ":lL:m:c:s" OPT; do
+  case "${OPT}" in
+    l) LOG="${DEFAULTLOG}" ;;
+    L) LOG="${OPTARG}" ;;
+    m)
+      if [ "${OPTARG}" ]; then
+        EMAIL="${OPTARG}"
+      else
+        print_usage
+      fi
+      ;;
+    c)
+      if [ "${OPTARG}" ]; then
+        MAXCAPACITY="${OPTARG}"
+      else
+        print_usage
+      fi
+      ;;
+    s) SUMMARY=yes ;;
+    *) print_usage ;;
+  esac
+done
+shift $((OPTIND-1))
+
+mail_report() { # email subject
+  if [ "${EMAIL}" ]; then
+    mail -s "zfs-check on `uname -n`: $*" "${EMAIL}" < "${REPORT}"
+  fi
+}
+
+write_log() {
+  if [ "${LOG}" ]; then
+    # Append to log file if it already exists, otherwise create it
+    cat $REPORT >> $LOG
+  else
+    cat $REPORT >&1
+  fi
   rm $REPORT
 }
 
-# Assess general zpool health status
-ZPOOLCONDITION=`zpool status -x`
-case $ZPOOLCONDITION in
-  "all pools are healthy")
-    echo -e "zpool health ok: $ZPOOLCONDITION" >> $REPORT
-    zpool status -v | grep -E '(pool:|scan:)' >> $REPORT
-    ;;
-  "no pools available")
-    echo -e "No pools mounted, aborting check\n" >> $REPORT
-    cat $REPORT >> $LOGFILE
-    mail_report "No ZFS pools mounted on `uname -n`"
-    exit 1
-    ;;
-  *)
-    echo -e "zpool health warning:\n`zpool status -v`" >> $REPORT
-    MAILNOTIFY=true
-    ;;
-esac
+#
+# main()
+#
+log summary "zfs-check started: `date`\n"
 
-# Check for drive errors
-DRIVEERRORS=`zpool status | grep ONLINE | grep -v state: | awk '{print $3 $4 $5}' | grep -v 000`
-if [ "$DRIVEERRORS" ]; then
-  echo -e "\nzpool errors:\n`zpool status | grep ONLINE | grep -v state:`\n" >> $REPORT
-  MAILNOTIFY=true
+# Show general zpool status and configuration
+# ZPOOLLIST=`sudo zpool list -H -o name`
+ZPOOLSTATUSV=`sudo zpool status -v`
+if [ "$ZPOOLSTATUSV" = "no pools available" ]; then
+  log warning "No pools mounted, aborting check"
+  mail_report "warning: no ZFS pools mounted"
+  write_log
+  exit 3
+fi
+log info "zpool status:\n${ZPOOLSTATUSV}\n"
+
+# Assess zpool health status
+ZPOOLCONDITION=`sudo zpool status -x`
+if [ "$ZPOOLCONDITION" = "all pools are healthy" ]; then
+  log summary "zfs-check info: ${ZPOOLCONDITION}\n"
+else
+  log warning "zpool health: ${ZPOOLCONDITION}\n"
+fi
+# Check for drive errors on ONLINE VDEVs
+VDEVERRORS=`echo "${ZPOOLSTATUSV}" \
+| awk '$1 != "state:" && $2 == "ONLINE" && $3 $4 $5 != "000"'`
+if [ "$VDEVERRORS" ]; then
+  log warning "vdev errors reported"
+  # Print title row
+  log info `echo "${ZPOOLSTATUSV}" | awk '$1 == "NAME"' | head -1`
+  log info "${VDEVERRORS}\n"
 fi
 
 # Assess zpool capacity utilisation
-echo -e "\n\nzpool utilisation:\n" >> $REPORT
-CAPACITY=`zpool list -H -o capacity`
-for line in ${CAPACITY//%/}
-do
-  if [ $line -ge $MAXCAPACITY ]; then
-    echo -e "Capacity exceeds $MAXCAPACITY% in a zpool\n" >> $REPORT
-    MAILNOTIFY=true
-  fi
-done
-zpool list >> $REPORT
-echo -e "\n\nvdev utilisation:\n" >> $REPORT
-zpool iostat -v >> $REPORT
-
-# Show zfs dataset usage
-echo -e "\n\nzfs dataset utilisation:\n" >> $REPORT
-zfs list >> $REPORT
-echo -e "\n\nzfs non-auto snapshot utilisation:\n" >> $REPORT
-ZPOOLLIST=`zpool list -H -o name`
-for pool in ${ZPOOLLIST}
-do
-  zfs list -t snapshot -r $pool | grep -v "zfs-auto-snap_"  >> $REPORT
-done
-echo -e "\n--------------------------------------------------------------------\n\n" >> $REPORT
-
-cat $REPORT >> $LOGFILE
-if [ $MAILNOTIFY = true ]; then
-  mail_report "ZFS health warning for `uname -n`"
-else
-  rm $REPORT
+log summary "zpool utilisation summary:\n`sudo zpool list`\n"
+CAPACITY=`sudo zpool list -H -o name,capacity,free`
+CAPWARN=`echo "${CAPACITY}" | awk -v max="${MAXCAPACITY}" \
+  '$2 > max { print "utilisation is "$2" in "$1" with "$3" free" }'`
+if [ "${CAPWARN}" ]; then
+  log warning "${CAPWARN}\n"
 fi
 
+# Show zfs dataset usage
+log info "zfs dataset utilisation:\n`sudo zfs list`\n"
+log info "zfs non-auto snapshot utilisation:"
+log info "`sudo zfs list -rt snapshot | grep -v ${ZFSAUTOSNAPLABEL}`\n"
+
+# End zfs-check
+log info "zfs-check finished: `date`"
+log info "---------------------------------------------------------------"
+if [ "${MAILNOTIFY}" ]; then
+  mail_report "ZFS health warning"
+fi
+write_log
